55750d4d63c1b4dd7e99e3d90283da2fd21b7dc7
Nina Tran <ninatranschacc@gmail.com>
2018-01-08 21:58:08 -0800

    Added some commonly-used NuGet packages (AngularJS); Tested Web API Controllers

---
+++ MyStarterApp.Web/Controllers/Api/DefaultTestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyStarterApp.Web.Controllers.Api
+{
+    [RoutePrefix("api/defaulttest")]
+    public class DefaultTestController : ApiController
+    {
+        [HttpGet, Route("{msg}")]
+        public HttpResponseMessage EchoItBack(string msg)
+        {
+            // Just making sure that the WEB API Controller works
+            // This file is only being used for testing purposes; not part of the main application
+            return Request.CreateResponse(HttpStatusCode.OK, msg);
+        }
+    }
+}
---
+++ MyStarterApp.Web/app/main/app.routes.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    "use strict";
+    angular
+        .module("mainApp" + ".routes", []);
+
+    app.config(_configureStates);
+
+    _configureStates.$inject = ["$stateProvider", "$locationProvider", "$urlRouterProvider"];
+
+    function _configureStates($stateProvider, $locationProvider, $urlRouterProvider) {
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+        $urlRouterProvider.otherwise("/home");
+        $stateProvider
+            .state({
+                name: "home",
+                url: "/home",
+                templateUrl: "/app/main/modules/home/home.html",
+                title: "Home Page",
+                controller: "homeController as homeCtrl"
+            })
+            ;
+    }
+})();
