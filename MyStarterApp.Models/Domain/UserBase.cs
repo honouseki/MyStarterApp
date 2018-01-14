e2633d83a7332508544d4db8c404450de1cb0cba
Nina Tran <ninatranschacc@gmail.com>
2018-01-13 19:57:55 -0800

    Added Services project and needed references

---
+++ MyStarterApp.Models/Domain/UserBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStarterApp.Models.Domain
+{
+    public class UserBase
+    {
+        public int UserId { get; set; }
+        public IEnumerable<string> Roles { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public bool Remember { get; set; }
+        public int RoleId { get; set; }
+    }
+}
