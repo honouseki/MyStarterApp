85935ddcba7ed955cbef09978708b15f7d84d1d4
Nina Tran <ninatranschacc@gmail.com>
2018-01-26 15:45:21 -0800

    Email service test setup complete

--- MyStarterApp.Services/EmailTestService.cs
+++ MyStarterApp.Services/EmailTestService.cs
@@ -5,23 +5,25 @@ using System.Net;
 using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace MyStarterApp.Services.Services
 {
+    // This will serve as the basic 'template' in the case an emailing service is desired for your particular application
     public class EmailTestService
     {
         public bool SendEmail()
         {
             bool sent = false;
 
-            // Instantiating the client
+            // Instantiating the client, using google's smtp server
             SmtpClient client = new SmtpClient();
             client.Host = "smtp.gmail.com";
             client.Port = 587;
             client.EnableSsl = true;
             client.UseDefaultCredentials = false;
             client.DeliveryMethod = SmtpDeliveryMethod.Network;
-            client.Credentials = new NetworkCredential("someGmail@gmail.com", "somePass1");
+            client.Credentials = new NetworkCredential("someCredibleEmail@mailinator.com", "somePass!");
 
             // The message to send
             MailMessage message = new MailMessage();
@@ -30,10 +32,15 @@ namespace MyStarterApp.Services.Services
             // Google requires the email address to be owned by YOU, so this is a set issue ^. There are work-arounds 
             // (May use other third-party email services instead, like SendGrid... I am too broke.)
             // In this case, I'm just using the below From to set the name
-            message.From = new MailAddress("someSenderAddress@mailinator.com", "My Name");
-            message.To.Add(new MailAddress("recipientAddress@@mailinator.com", "Their Name"));
+            message.From = new MailAddress("someCredibleEmail@mailinator.com", "Kazeoseki");
+            message.To.Add(new MailAddress("carrot@mailinator.com", "Carrot"));
             message.Subject = "TEST EMAIL! With SMTP...";
-            message.Body = "<h1>Some header</h1><p>Some paragraph..?</p><p>This should show on another line</p><i>And this should be italicized...<i>";
+            // message.Body = "<h1>Some header</h1><p>Some paragraph..?</p><p>This should show on another line</p><i>And this should be italicized...<i>";
+
+            // Attempting to use an external html file for the contents
+            string newBody = HttpContext.Current.Server.MapPath("~/Email_Templates/email_confirmation.html");
+            newBody = System.IO.File.ReadAllText(newBody);
+            message.Body = newBody;
             message.IsBodyHtml = true;
 
             // Sending the message through the client
---
+++ MyStarterApp.Web/app/main/email_test/emailTest.js
@@ -0,0 +1,70 @@
+﻿// Have the service and the controller here...just to run it. And the component ()
+// This is just to test being able to send an email from the front-end
+
+// Component
+(function () {
+    "use strict";
+    angular
+        .module("mainApp")
+        .component("emailTestComponent", {
+            templateUrl: "/app/main/email_test/emailTest.html",
+            controller: "emailTestController"
+        });
+})();
+
+// Controller
+(function () {
+    "use strict";
+    angular
+        .module("mainApp")
+        .controller("emailTestController", EmailTestController);
+
+    EmailTestController.$inject = ["$scope", "emailTestService"];
+
+    function EmailTestController($scope, EmailTestService) {
+        var vm = this;
+        vm.$scope = $scope;
+        vm.$onInit = _onInit;
+        vm.emailTestService = EmailTestService;
+        vm.sendEmail = _sendEmail;
+
+        function _onInit() {
+            console.log("EmailTestController");
+        }
+
+        function _sendEmail() {
+            vm.emailTestService.emailTest()
+                .then(function (res) { console.log(res); })
+                .catch(function (err) { console.log(err); });
+        }
+    }
+})();
+
+// Service
+(function () {
+    "use strict";
+    angular
+        .module('mainApp')
+        .factory("emailTestService", EmailTestService);
+
+    EmailTestService.$inject = ["$http", "$q"];
+
+    function EmailTestService($http, $q) {
+        return {
+            emailTest: _emailTest
+        };
+
+        function _emailTest() {
+            return $http.get("/api/defaulttest/emailtest", { withCredentials: true })
+                .then(success).catch(error);
+        }
+
+        function success(res) {
+            return res;
+        }
+
+        function error(err) {
+            return $q.reject(err);
+        }
+    }
+})();
